eaa2102032d75bad57be42ccab65ed504dcb11c7
Kangze Huang <kangzeroo@gmail.com>
2017-12-23 02:27:26 -0500

    all sms logged

--- message_logs/schema/dynamodb_tablenames.js
+++ message_logs/schema/dynamodb_tablenames.js
@@ -1,2 +1,7 @@
 
 exports.COMMUNICATIONS_HISTORY = 'Communication_Logs'
+exports.ORCHESTRA_ACTIVITY = 'Orchestra_Activity'
+
+
+// not actually a table. Just the identifier value for a message coming from RentHero
+exports.RENTHERO_SENDER_ID = 'RENTHERO_ORCHESTRA'
