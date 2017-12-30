c4585652ae8c76c43537cb702e37becf56fc3d0c
Kangze Huang <kangzeroo@gmail.com>
2017-12-30 17:36:27 -0500

    changed index of communications_logs table

--- routes/sms_routes.js
+++ routes/sms_routes.js
@@ -145,9 +145,9 @@ const sendInitialSMSForExistingTenantLandlordPair = (info, landlord, tenant, twi
       'LANDLORD_PHONE': landlord.landlordPhone,
 
       'PROXY_CONTACT_ID': twilioPhone,
-      'SENDER_ID': landlord.landlordPhone,
+      'SENDER_ID': RENTHERO_SENDER_ID,
       'RECEIVER_ID': tenant.tenantId,
-      'SENDER_CONTACT_ID': RENTHERO_SENDER_ID,
+      'SENDER_CONTACT_ID': landlord.landlordPhone,
       'RECEIVER_CONTACT_ID': tenant.tenantPhone,
 
       'TEXT': tenantBody,
@@ -185,9 +185,9 @@ const sendInitialSMSForExistingTenantLandlordPair = (info, landlord, tenant, twi
       'LANDLORD_PHONE': landlord.landlordPhone,
 
       'PROXY_CONTACT_ID': twilioPhone,
-      'SENDER_ID': tenant.tenantPhone,
+      'SENDER_ID': RENTHERO_SENDER_ID,
       'RECEIVER_ID': landlord.landlordId,
-      'SENDER_CONTACT_ID': RENTHERO_SENDER_ID,
+      'SENDER_CONTACT_ID': tenant.tenantPhone,
       'RECEIVER_CONTACT_ID': landlord.landlordPhone,
 
       'TEXT': landlordBody,
@@ -232,9 +232,9 @@ const sendInitialSMSToTenantAndLandlord = (info, landlord, tenant, twilioPhone) 
       'LANDLORD_PHONE': landlord.landlordPhone,
 
       'PROXY_CONTACT_ID': twilioPhone,
-      'SENDER_ID': landlord.landlordPhone,
+      'SENDER_ID': RENTHERO_SENDER_ID,
       'RECEIVER_ID': tenant.tenantId,
-      'SENDER_CONTACT_ID': RENTHERO_SENDER_ID,
+      'SENDER_CONTACT_ID': landlord.landlordPhone,
       'RECEIVER_CONTACT_ID': tenant.tenantPhone,
 
       'TEXT': tenantBody,
@@ -272,9 +272,9 @@ const sendInitialSMSToTenantAndLandlord = (info, landlord, tenant, twilioPhone) 
       'LANDLORD_PHONE': landlord.landlordPhone,
 
       'PROXY_CONTACT_ID': twilioPhone,
-      'SENDER_ID': tenant.tenantPhone,
+      'SENDER_ID': RENTHERO_SENDER_ID,
       'RECEIVER_ID': landlord.landlordId,
-      'SENDER_CONTACT_ID': RENTHERO_SENDER_ID,
+      'SENDER_CONTACT_ID': tenant.tenantPhone,
       'RECEIVER_CONTACT_ID': landlord.landlordPhone,
 
       'TEXT': landlordBody,
