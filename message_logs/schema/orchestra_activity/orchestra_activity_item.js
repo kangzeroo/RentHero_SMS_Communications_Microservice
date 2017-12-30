00923e8513b58b0cc9aa60ee1015fadf20b07280
Kangze Huang <kangzeroo@gmail.com>
2017-12-30 12:24:21 -0500

    magiclinks are tracked in dynamodb

--- message_logs/schema/orchestra_activity/orchestra_activity_item.js
+++ message_logs/schema/orchestra_activity/orchestra_activity_item.js
@@ -1,180 +1,74 @@
-const COMMUNICATIONS_HISTORY = require('../dynamodb_tablenames').COMMUNICATIONS_HISTORY
+const ORCHESTRA_ACTIVITY = require('../dynamodb_tablenames').ORCHESTRA_ACTIVITY
 
 
 // ====================================
 
 exports.reference_items = [
   {
-    'TableName': COMMUNICATIONS_HISTORY,
-    'Item': {
-      'ACTION': 'INITIAL_TOUR_REQUEST',
-      'DATE': new Date().getTime(),
-      'ORCHESTRA_ID': 'uuid.v4()',
-
-      'TENANT_ID': `this.props.tenant_profile.id` || 'NONE',
-      'TENANT_PHONE': '+134534536565',
-      'TENANT_NAME': 'Steve Carrol',
-      'LANDLORD_ID': '348tuoudfsljf',
-      'LANDLORD_NAME': 'KW2Rent',
-      'LANDLORD_PHONE': '+14556485767',
-
-      'PROXY_CONTACT_ID': '983LIJSDFSDFLJ9',
-      'SENDER_ID': 'SGF4534536565',
-      'RECEIVER_ID': 'SDF4556485767',
-      'SENDER_CONTACT_ID': 'Rentburrow',
-      'RECEIVER_CONTACT_ID': '+14556485767 but sent to both, so two records would occur',
-
-      'TEXT': 'Hello I would like to book a tour for _______ on ______. Would this work?',
-      'BUILDING_ID': '394dfhglf8348to',
-      'BUILDING_ADDRESS': '330 King St North, Waterloo ON',
-    }
-  },
-  {
-    'TableName': COMMUNICATIONS_HISTORY,
-    'Item': {
-      'ACTION': 'INITIAL_MESSAGE',
-      'DATE': new Date().getTime(),
-      'ORCHESTRA_ID': 'uuid.v4()',
-
-      'TENANT_ID': `this.props.tenant_profile.id` || 'NONE',
-      'TENANT_NAME': 'Steve Carrol',
-      'TENANT_PHONE': '+134534536565',
-      'LANDLORD_ID': '348tuoudfsljf',
-      'LANDLORD_NAME': 'KW2Rent',
-      'LANDLORD_PHONE': '+14556485767',
-
-      'PROXY_CONTACT_ID': '983LIJSDFSDFLJ9',
-      'SENDER_ID': 'SGF4534536565',
-      'RECEIVER_ID': 'SDF4556485767',
-      'SENDER_CONTACT_ID': 'Rentburrow',
-      'RECEIVER_CONTACT_ID': '+14556485767 but sent to both, so two records would occur',
-
-      'TEXT': 'Hello is this the landlord for XXXX?',
-      'BUILDING_ID': '394dfhglf8348to',
-      'BUILDING_ADDRESS': '330 King St North, Waterloo ON',
-    }
-  },
-  {
-    'TableName': COMMUNICATIONS_HISTORY,
-    'Item': {
-      'ACTION': 'SMS_MESSAGE',
-      'DATE': new Date().getTime(),
-      'ORCHESTRA_ID': 'uuid.v4()',
-
-      'PROXY_CONTACT_ID': '983LIJSDFSDFLJ9',
-      'SENDER_ID': 'SGF4534536565',
-      'RECEIVER_ID': 'SDF4556485767',
-      'SENDER_CONTACT_ID': '+134534536565',
-      'RECEIVER_CONTACT_ID': '+14556485767',
-
-      'TEXT': 'Yes there is availability...',
-    }
-  },
-  {
-    'TableName': COMMUNICATIONS_HISTORY,
-    'Item': {
-      'ACTION': 'SENT_GROUP_INVITE',
-      'DATE': new Date().getTime(),
-      'ORCHESTRA_ID': 'uuid.v4()',
-
-      'PROXY_CONTACT_ID': '983LIJSDFSDFLJ9',
-      'SENDER_ID': `this.props.tenant_profile.id` || 'NONE',
-      'RECEIVER_ID': '+14556485767',
-      'SENDER_CONTACT_ID': '+134534536565',
-      'RECEIVER_CONTACT_ID': '+14556485767',
-
-      'TEXT': 'Yes there is availability...',
-      'GROUP_ID': 'LSDJF45OFS456FD',
-      'INVITATION_ID': 'LJSDFJLSKDFS3849',
-    }
-  },
-  {
-    'TableName': COMMUNICATIONS_HISTORY,
-    'Item': {
-      'ACTION': 'SENT_RECOMMENDATION',
-      'DATE': new Date().getTime(),
-      'ORCHESTRA_ID': 'uuid.v4()',
-
-      'PROXY_CONTACT_ID': '983LIJSDFSDFLJ9',
-      'SENDER_ID': `this.props.tenant_profile.id` || 'NONE',
-      'RECEIVER_ID': '+14556485767',
-      'SENDER_CONTACT_ID': '+134534536565',
-      'RECEIVER_CONTACT_ID': '+14556485767',
-
-      'TEXT': 'Hello Sarah, check out this link for recommendations',
-      'RECOMMENDATION_DETAILS': '4 bed places with ensuite - 110 columbia with id...',
-    }
-  },
-  {
-    'TableName': COMMUNICATIONS_HISTORY,
+    'TableName': ORCHESTRA_ACTIVITY,
     'Item': {
       'ACTION': 'MAGIC_LINK_GROUP_SENT',
       'DATE': new Date().getTime(),
       'ORCHESTRA_ID': 'uuid.v4()',
+      'MAGIC_LINK_ID': 'jolasf7iu8s8auf9asf',
 
-      'SENDER_ID': `tenant_id`,
-      'RECEIVER_ID': 'tenant_id',
-      'SENDER_CONTACT_ID': '+134534536565',
-      'RECEIVER_CONTACT_ID': '+14556485767',
+      'TARGET_ID': 'tenant_id',
+      'TARGET_CONTACT_ID': '+14556485767',
       'PROXY_CONTACT_ID': '983LIJSDFSDFLJ9',
 
+      'SENDER_ID': `tenant_id`,
+      'SENDER_CONTACT_ID': '+134534536565',
       'GROUP_ID': 'LSDJF45OFS456FD',
       'INVITATION_ID': 'LJSDFJLSKDFS3849',
-      'MAGIC_LINK_ID': 'jolasf7iu8s8auf9asf'
+      'FINGERPRINT': 'this.props.fingerprint',
     }
   },
   {
-    'TableName': COMMUNICATIONS_HISTORY,
+    'TableName': ORCHESTRA_ACTIVITY,
     'Item': {
-      'ACTION': 'MAGIC_LINK_GROUP_OPENED',
+      'ACTION': 'MAGIC_LINK_GROUP_PROGRESS',
       'DATE': new Date().getTime(),
       'ORCHESTRA_ID': 'uuid.v4()',
+      'MAGIC_LINK_ID': 'jolasf7iu8s8auf9asf',
 
-      'SENDER_ID': `tenant_id`,
-      'RECEIVER_ID': 'tenant_id',
-      'SENDER_CONTACT_ID': '+134534536565',
-      'RECEIVER_CONTACT_ID': '+14556485767',
-      'PROXY_CONTACT_ID': '983LIJSDFSDFLJ9',
+      'TARGET_ID': 'tenant_id',
+      'TARGET_CONTACT_ID': '+14556485767',
 
-      'GROUP_ID': 'LSDJF45OFS456FD',
-      'INVITATION_ID': 'LJSDFJLSKDFS3849',
-      'MAGIC_LINK_ID': 'jolasf7iu8s8auf9asf'
+      'STEP': 'OPENED, STEP1, STEP2, COMPLETED',
+      'FINGERPRINT': 'this.props.fingerprint',
     },
   },
   {
-    'TableName': COMMUNICATIONS_HISTORY,
+    'TableName': ORCHESTRA_ACTIVITY,
     'Item': {
       'ACTION': 'MAGIC_LINK_RECOMMENDATION_SENT',
       'DATE': new Date().getTime(),
       'ORCHESTRA_ID': 'uuid.v4()',
+      'MAGIC_LINK_ID': 'jolasf7iu8s8auf9asf',
 
-      'SENDER_ID': `RENTHERO_ORCHESTRA`,
-      'RECEIVER_ID': 'tenant_id',
-      'SENDER_CONTACT_ID': 'RENTHERO_ORCHESTRA',
-      'RECEIVER_CONTACT_ID': '+14556485767',
+      'TARGET_ID': 'tenant_id',
+      'TARGET_CONTACT_ID': '+14556485767',
       'PROXY_CONTACT_ID': '983LIJSDFSDFLJ9',
 
-      'GROUP_ID': 'LSDJF45OFS456FD',
-      'INVITATION_ID': 'LJSDFJLSKDFS3849',
-      'MAGIC_LINK_ID': 'jolasf7iu8s8auf9asf'
+      'SENDER_ID': `RENTHERO_ORCHESTRA`,
+      'SENDER_CONTACT_ID': 'RENTHERO_ORCHESTRA',
+      'LEAD_ID': 'asdfgd490jfjsdfsf',
+      'FINGERPRINT': 'this.props.fingerprint',
     }
   },
   {
-    'TableName': COMMUNICATIONS_HISTORY,
+    'TableName': ORCHESTRA_ACTIVITY,
     'Item': {
-      'ACTION': 'MAGIC_LINK_RECOMMENDATION_OPENED',
+      'ACTION': 'MAGIC_LINK_RECOMMENDATION_PROGRESS',
       'DATE': new Date().getTime(),
       'ORCHESTRA_ID': 'uuid.v4()',
+      'MAGIC_LINK_ID': 'jolasf7iu8s8auf9asf',
 
-      'SENDER_ID': `tenant_id`,
-      'RECEIVER_ID': 'tenant_id',
-      'SENDER_CONTACT_ID': '+134534536565',
-      'RECEIVER_CONTACT_ID': '+14556485767',
-      'PROXY_CONTACT_ID': '983LIJSDFSDFLJ9',
+      'TARGET_ID': 'tenant_id',
+      'TARGET_CONTACT_ID': '+14556485767',
 
-      'GROUP_ID': 'LSDJF45OFS456FD',
-      'INVITATION_ID': 'LJSDFJLSKDFS3849',
-      'MAGIC_LINK_ID': 'jolasf7iu8s8auf9asf'
+      'STEP': 'OPENED, STEP1, STEP2, COMPLETED',
+      'FINGERPRINT': 'this.props.fingerprint',
     }
   }
 ]
