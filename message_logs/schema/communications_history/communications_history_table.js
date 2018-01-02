8b4685971ba30f9a0eb210102a2363d1fec02873
Kangze Huang <kangzeroo@gmail.com>
2018-01-02 10:53:50 -0500

    fixed stuff

--- message_logs/schema/communications_history/communications_history_table.js
+++ message_logs/schema/communications_history/communications_history_table.js
@@ -9,50 +9,29 @@ const communicationsHistoryTableParams = {
     KeySchema: [
         // USE CASE: ALLOWS ME TO SEE ALL USER PREFERENCES INTEL IN CHRONOLOGICAL ORDER. EG: USER LOOKS FOR ENSUITE FIRST BEFORE CHANGING THEIR FILTERS TO LOOK FOR LESS ROOMATES NO ENSUITE
         { AttributeName: "SENDER_CONTACT_ID", KeyType: "HASH" },  //Partition key
-        { AttributeName: "DATE", KeyType: "RANGE" },  //Sort key
+        { AttributeName: "COMMUNICATION_ID", KeyType: "RANGE" },  //Sort key
     ],
     AttributeDefinitions: [
         { AttributeName: "SENDER_CONTACT_ID", AttributeType: "S" },       // a phone, email, fb messenger id (of the sender)
         { AttributeName: "PROXY_CONTACT_ID", AttributeType: "S" },        // a twilio number, a email forwarder client, a facebook chat thread
         { AttributeName: "RECEIVER_CONTACT_ID", AttributeType: "S" },     // a phone, email, fb messenger id (of the receiver)
-        { AttributeName: "DATE", AttributeType: "N" },
         { AttributeName: "ACTION", AttributeType: "S" },
         { AttributeName: "SENDER_ID", AttributeType: "S" },
         { AttributeName: "RECEIVER_ID", AttributeType: "S" },
         { AttributeName: "COMMUNICATION_ID", AttributeType: "S" },
+        { AttributeName: "MEDIUM", AttributeType: "S" },
     ],
     ProvisionedThroughput: {
         ReadCapacityUnits: 1,
         WriteCapacityUnits: 5,
     },
-    LocalSecondaryIndexes: [
-      {
-        // USE CASE: ALLOWS ME TO SHOW ALL USER PREFERENCES INTEL GROUPED BY ACTION. EG: I CAN SHOW THE TRENDS OF A USER ADJUSTING THEIR RENT PRICE THROUGHOUT THEIR 4 YEARS IN UNIVERSITY
-        IndexName: 'By_Local_Sender_vs_Receiver', /* required */
-        KeySchema: [ /* required */
-          {
-            AttributeName: 'SENDER_CONTACT_ID', /* required */
-            KeyType: 'HASH' /* required */
-          },
-          {
-            AttributeName: 'RECEIVER_CONTACT_ID', /* required */
-            KeyType: 'RANGE' /* required */
-          }
-          /* more items */
-        ],
-        Projection: { /* required */
-          ProjectionType: 'ALL'
-        }
-      },
-      /* more items */
-    ],
     GlobalSecondaryIndexes: [
       {
         // USE CASE: ALLOWS ME TO SEE ALL INTEL OF A SPECIFIC ACTION, GROUPED BY USERS. EG: SHOW ME ALL PRICE ADJUSTMENTS, AND NOW I CAN GROUP USER POPULATIONS INTO PRICE RANGES.
         IndexName: 'By_Action', /* required */
         KeySchema: [ /* required */
           {AttributeName: 'ACTION', KeyType: 'HASH'},
-          {AttributeName: 'DATE', KeyType: 'RANGE'}
+          {AttributeName: 'COMMUNICATION_ID', KeyType: 'RANGE'}
         ],
         Projection: { /* required */
           ProjectionType: 'ALL'
@@ -67,7 +46,7 @@ const communicationsHistoryTableParams = {
         IndexName: 'By_RECEIVER_ID', /* required */
         KeySchema: [ /* required */
           {AttributeName: 'RECEIVER_ID', KeyType: 'HASH'},
-          {AttributeName: 'DATE', KeyType: 'RANGE'}
+          {AttributeName: 'COMMUNICATION_ID', KeyType: 'RANGE'}
         ],
         Projection: { /* required */
           ProjectionType: 'ALL'
@@ -82,7 +61,7 @@ const communicationsHistoryTableParams = {
         IndexName: 'By_SENDER_ID', /* required */
         KeySchema: [ /* required */
           {AttributeName: 'SENDER_ID', KeyType: 'HASH'},
-          {AttributeName: 'DATE', KeyType: 'RANGE'}
+          {AttributeName: 'COMMUNICATION_ID', KeyType: 'RANGE'}
         ],
         Projection: { /* required */
           ProjectionType: 'ALL'
@@ -97,7 +76,7 @@ const communicationsHistoryTableParams = {
         IndexName: 'By_PROXY_CONTACT_ID', /* required */
         KeySchema: [ /* required */
           {AttributeName: 'PROXY_CONTACT_ID', KeyType: 'HASH'},
-          {AttributeName: 'DATE', KeyType: 'RANGE'}
+          {AttributeName: 'COMMUNICATION_ID', KeyType: 'RANGE'}
         ],
         Projection: { /* required */
           ProjectionType: 'ALL'
@@ -109,10 +88,10 @@ const communicationsHistoryTableParams = {
       },
       {
         // USE CASE: ALLOWS ME TO SEE ALL INTEL OF A SPECIFIC ACTION, GROUPED BY USERS. EG: SHOW ME ALL PRICE ADJUSTMENTS, AND NOW I CAN GROUP USER POPULATIONS INTO PRICE RANGES.
-        IndexName: 'By_COMMUNICATION_ID', /* required */
+        IndexName: 'By_MEDIUM', /* required */
         KeySchema: [ /* required */
-          {AttributeName: 'COMMUNICATION_ID', KeyType: 'HASH'},
-          {AttributeName: 'DATE', KeyType: 'RANGE'}
+          {AttributeName: 'MEDIUM', KeyType: 'HASH'},
+          {AttributeName: 'COMMUNICATION_ID', KeyType: 'RANGE'}
         ],
         Projection: { /* required */
           ProjectionType: 'ALL'
