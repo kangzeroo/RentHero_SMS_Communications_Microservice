2b3a938da9ccaa3cfbab82126ba77da0e90a2f83
Kangze Huang <kangzeroo@gmail.com>
2018-01-02 11:06:14 -0500

    removed creds

--- api/ses_api.js
+++ api/ses_api.js
@@ -27,20 +27,18 @@ exports.generateInitialEmail = function(toEmailAddresses, proxyEmailAddress, ten
 		} else {
 			const params = createInitialParams(toEmailAddresses, proxyEmailAddress, tenant, message, building, tenantOrLandlord)
 			// console.log('Sending email with attached params!')
-			AWS.config.credentials.refresh(function() {
-				// console.log(AWS.config.credentials)
-				ses.sendEmail(params, function(err, data) {
-				  if (err) {
-				  	 console.log(err); // an error occurred
-				  	 rej(err)
-				  } else {
-				  	// console.log(data);           // successful response
-  					res({
-              message: 'Success! Initial mail sent',
-              data: data,
-            })
-          }
-				})
+			// console.log(AWS.config.credentials)
+			ses.sendEmail(params, function(err, data) {
+			  if (err) {
+			  	 console.log(err); // an error occurred
+			  	 rej(err)
+			  } else {
+			  	// console.log(data);           // successful response
+					res({
+            message: 'Success! Initial mail sent',
+            data: data,
+          })
+        }
 			})
 		}
 	})
