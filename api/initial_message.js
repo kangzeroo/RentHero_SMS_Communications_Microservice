a4f7c7da5515abf16f2c894fbebdf0d171c9201c
Kangze Huang <kangzeroo@gmail.com>
2018-01-06 05:46:11 -0500

    other

--- api/initial_message.js
+++ api/initial_message.js
@@ -2,8 +2,8 @@
 exports.generateInitialMessageBody_Tenant = function(info, landlord_name, message_id){
   const p = new Promise((res, rej) => {
     res(`
-      [ VERIFIED RENTHERO MESSAGE: RentHero.cc/m/${message_id} ]
       Hello ${info.first_name}, you can now text the landlord directly here. The landlord, ${landlord_name}, has also recieved a text regarding your interest in ${info.building_address} with your group size of ${info.group_size}.
+      [ VERIFIED RENTHERO MESSAGE: RentHero.cc/m/${message_id} ]
     `)
   })
   return p
@@ -13,8 +13,8 @@ exports.generateInitialMessageBody_Tenant = function(info, landlord_name, messag
 exports.generateInitialMessageBody_Landlord = function(info, landlord_name, message_id){
   const p = new Promise((res, rej) => {
     res(`
-      [ VERIFIED RENTHERO MESSAGE: RentHero.cc/m/${message_id} ]
       Hello ${landlord_name}, Rentburrow.com has brought you a group of ${info.group_size} students led by ${info.first_name}. ${info.gender === 'Male' ? 'His' : 'Her'} group is interested in ${info.building_address}. You can text ${info.gender === 'Male' ? 'him' : 'her'} back directly here. In the notes, ${info.first_name} said: "${info.group_notes}".
+      [ VERIFIED RENTHERO MESSAGE: RentHero.cc/m/${message_id} ]
     `)
   })
   return p
@@ -24,8 +24,8 @@ exports.generateInitialMessageBody_Landlord = function(info, landlord_name, mess
 exports.generateInitialMessageBody_Tenant_ForExistingPair = function(info, landlord_name, message_id){
   const p = new Promise((res, rej) => {
     res(`
-      [ VERIFIED RENTHERO MESSAGE: RentHero.cc/m/${message_id} ]
       Hello ${info.first_name}. ${info.building_address} is also managed by ${landlord_name}.
+      [ VERIFIED RENTHERO MESSAGE: RentHero.cc/m/${message_id} ]
     `)
   })
   return p
@@ -35,8 +35,8 @@ exports.generateInitialMessageBody_Tenant_ForExistingPair = function(info, landl
 exports.generateInitialMessageBody_Landlord_ForExistingPair = function(info, landlord_name, message_id){
   const p = new Promise((res, rej) => {
     res(`
-      [ VERIFIED RENTHERO MESSAGE: RentHero.cc/m/${message_id} ]
       Hello ${landlord_name}, ${info.first_name} on RentHero.ca is also interested in ${info.building_address}. Notes: ${info.group_notes}
+      [ VERIFIED RENTHERO MESSAGE: RentHero.cc/m/${message_id} ]
     `)
   })
   return p
