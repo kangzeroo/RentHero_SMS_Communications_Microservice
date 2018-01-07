d78b2e2d44c7152187bc6c1108869e2d19605047
Jimmy <j63guo@uwaterloo.ca>
2018-01-07 03:18:12 -0500

    asd

--- api/initial_message.js
+++ api/initial_message.js
@@ -13,7 +13,7 @@ exports.generateInitialMessageBody_Tenant = function(info, landlord_name, messag
 exports.generateInitialMessageBody_Landlord = function(info, landlord_name, message_id){
   const p = new Promise((res, rej) => {
     res(`
-      Hello ${landlord_name}, Rentburrow.com has brought you a group of ${info.group_size} students led by ${info.first_name}. ${info.gender === 'Male' ? 'His' : 'Her'} group is interested in ${info.building_address}. You can text ${info.gender === 'Male' ? 'him' : 'her'} back directly here. In the notes, ${info.first_name} said: "${info.group_notes}".
+      Hello ${landlord_name}, Rentburrow.com has brought you a group of ${info.group_size} students led by ${info.first_name}. The group is interested in ${info.building_address}. You can text ${info.first_name} back directly here. In the notes, ${info.first_name} said: "${info.group_notes}".
       [ VERIFIED RENTHERO MESSAGE: RentHero.cc/m/${message_id} ]
     `)
   })
