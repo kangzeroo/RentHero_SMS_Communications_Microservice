531c2b2a230cef91517dc32a7aacecb2cd8cbc94
Jimmy <j63guo@uwaterloo.ca>
2018-01-30 21:41:19 -0500

    asd

--- api/corporate_landlord_api.js
+++ api/corporate_landlord_api.js
@@ -13,15 +13,7 @@ if (process.env.NODE_ENV === 'production') {
   LANDLORD_RESPONSIVENESS_MICROSERVICE = 'https://renthero.host:3009'
 }
 
-exports.generateInitialCorporateEmail = function(toEmailAddresses, proxyFromEmailAddress, tenant, tenantMessage, landlordMessage, building, suite, tenantOrLandlord){
-  /*
-    toEmailAddresses = ['personA@email.com', 'personB@email.com']
-    proxyFromEmailAddress = 'relationshipID@renthero.cc',
-    tenant = { tenant_id: '89oOHDF9f', first_name: 'Mike' }
-    message = 'Hello, Renthero has generated a lead for you...'
-    building = { building_id, building_alias }
-    tenantOrLandlord = 'landlord' || 'tenant'
-  */
+exports.generateInitialCorporateEmail = function(toEmailAddresses, proxyFromEmailAddress, tenant, tenantMessage, landlordMessage, building, suite){
   console.log('======> toEmailAddresses: ', toEmailAddresses)
   console.log('======> proxyFromEmailAddress: ', proxyFromEmailAddress)
   console.log('======> tenant: ', tenant)
